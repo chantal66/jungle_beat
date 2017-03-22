2553a70937d084405df5b14d2a6fc579266a1111
Chantal Justamond <cjustamm66@me.com>
2017-03-21 22:08:15 -0600

    count can count + 2

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,6 +4,7 @@ class LinkedList
   def initialize
     @head = nil
     @count = 0
+    
   end
 
   # def head
@@ -22,18 +23,36 @@ class LinkedList
     end  
   end
   def count
-    if @head.nil?
-      current_node = @head
-      @count +=1
+    if @head
+       current_node = @head
+       @count +=1
     else 
-      until current_node.next_node.nil?
+        @count
+        until current_node.next_node.nil?
+          current_node = current_node.next_node
+          @count
+        end
+        #current_node.next_node = Node.new(string)
+    end  
+    
+  
+  end
+  def to_string
+    @result = ''
+    if @head
+      current_node = @head
+      @result << current_node.data
+      
+    else 
+      @result
+      until current_node.next_node.nil?  #I removed .next_node from my until given an error
         current_node = current_node.next_node
-        @count +=1
+        @result << current_node.data 
+        
       end
       #current_node.next_node = Node.new(string)  
-      @count
+      @result
     end
-  
   end
 
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -2,16 +2,19 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/linked_list'
 require './lib/node'
+require 'pry'
 
 class LinkedListTest < Minitest::Test
  
   def test_does_it_know_that_head_is_nil
     list = LinkedList.new
+
     assert_nil nil, list.head
   end
   def test_does_it_know_how_to_append_one_node
     list = LinkedList.new
     list.append('doop')
+
     assert_equal 'doop', list.head.data 
     
   end
@@ -21,6 +24,7 @@ class LinkedListTest < Minitest::Test
     list = LinkedList.new
     list.append('wwooow')
     list.append('hahaha')
+
     assert_equal 'wwooow', list.head.data
     assert_equal 'hahaha', list.head.next_node.data
   end
@@ -29,15 +33,35 @@ class LinkedListTest < Minitest::Test
     list.append('wwooow')
     list.append('hahaha')
     list.append('awesome')
+
     assert_equal 'awesome', list.head.next_node.next_node.data
   end  
     
   def test_can_it_count
     list = LinkedList.new
     #require 'pry'; binding.pry
+    
     list.append('wwooow')
-    assert_equal 1, list.count
-   
+    total_1 = list.count
+    list.append('doop')
+    total_2 = list.count
+    
+    assert_equal 1, total_1
+    assert_equal 2, total_2
+
+
+  end
+  def test_accepts_strings
+    list = LinkedList.new
+    
+    list.append('doop')
+    result_1 = list.to_string
+    
+    list.append('deep')
+    result_2 = list.to_string
+    #binding.pry
+    assert_equal 'doop', result_1
+    assert_equal 'deep', result_2
   end
   
   
