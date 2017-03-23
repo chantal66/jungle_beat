0d513793f4ef5a68d7eeb33de60ec11459d0c2cf
Chantal Justamond <cjustamm66@me.com>
2017-03-22 21:55:16 -0600

    jungle beat know its head

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -3,12 +3,39 @@ require './lib/node'
 
 class JungleBeat < LinkedList
   
-  attr_accessor :list
+  attr_accessor :list, :beat, :rate, :voice
 
   def initialize
     @list = LinkedList.new
+    @beat = beat
+
   end
 
-  
-  
+  def append(beat)
+    beat_split = beat.split(' ')
+    first = Node.new (beat_split[0])
+    
+    beat_split[0..-1].each do |beat|
+      @list.append(beat)
+    end
+
+  end
+
+  def count
+    @list.count
+  end
+
+  def play
+    puts `say -r #{rate} -v #{voice} #{beat}`
+    count
+  end
+
+  def rate
+    @rate = 500
+  end
+  def voice
+    @voice = 'Boing'
+    
+  end
+
 end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -13,9 +13,43 @@ class JungleBeatTest < Minitest::Test
 
   def test_can_it_append
     jb = JungleBeat.new
-    jb.append("deep doo ditt")
-
-    assert_equal 'deep doo ditt', jb.append
+    result = jb.append("deep doo ditt")
+   
+    assert_equal 'deep doo ditt', result
+    assert_equal 'deep', jb.list.head.data
+    assert_equal 'doo', jb.list.head.next_node.data
+      
   end
+
+  def test_can_it_append_many_many_things
   
+    jb = JungleBeat.new
+    jb.append("deep doo ditt")
+    jb.append("woo hoo shu")
+    
+    assert_equal 6, jb.count 
+   
+  end
+  def test_it_sets_a_rate
+    skip
+    jb = JungleBeat.new
+    jb.append("deep doo ditt woo hoo shu")
+    jb.count
+    jb.list
+    jb.play
+    jb.rate
+
+    assert_equal 500 , jb.rate
+    assert_equal 6, jb.count
+  end
+  def test_knows_the_beat
+    skip
+    jb = JungleBeat.new
+    jb.append("deep doo ditt deep")
+    
+    assert_equal 4, jb.play
+  end
+
+
+
 end
